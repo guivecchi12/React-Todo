93ae6e47a53b066016e77ed5f8460f1678ba76c7
unknown <guivecchi12@gmail.com>
2020-06-03 19:41:07 -0600

    new MVP

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -14,6 +14,7 @@ class TodoForm extends React.Component {
 
     submitTask = e =>{
         e.preventDefault();
+        this.setState({ task: '' });
         {this.props.addTodo( e, this.state.task )};
     }
 
